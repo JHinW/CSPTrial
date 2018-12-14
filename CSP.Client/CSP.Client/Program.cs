c5ae3d1aafcce4d295469f0d0ba703ce367a0584
Wang Jin3 <wang.jin3@oe.21vianet.com>
2018-12-14 14:37:12 +0800

    csp sample

---
+++ CSP.Client/CSP.Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CSP.Client
+{
+    /// <summary>
+    /// https://partner.partnercenter.microsoftonline.cn/commerce/preferredoffers/list
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new CSP.Client.Auth.Client();
+            Task.Run( async () =>
+            {
+                var users = await client.Test();
+                Console.WriteLine(users);
+
+                //var referrals = await client.TestReferrals();
+                //Console.WriteLine(referrals);
+
+                //var indirectresellers = await client.TestIndirectresellers();
+                //Console.WriteLine(indirectresellers);
+
+
+
+
+                //var usage = await client.TestPartnerUsage();
+                //Console.WriteLine(usage);
+
+
+                var json = await client.TestDeployment();
+                Console.WriteLine(json);
+
+                var dep = await client.TestCommercialDeployment();
+                Console.WriteLine(dep);
+
+
+                var usage = await client.TestCommercialUsage();
+                Console.WriteLine(usage);
+            }).Wait();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
