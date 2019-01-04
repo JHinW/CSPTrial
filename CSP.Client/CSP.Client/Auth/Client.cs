5cb88a5a486a29f1d5b23fe89043f1137cc35625
jin wang <v-jiwag@hotmail.com>
2019-01-04 14:09:28 +0800

    Jiwag (#1)
    * csp sample
    
    * init
    
    * add more scenario
    
    * config

---
+++ CSP.Client/CSP.Client/Auth/Client.cs
@@ -0,0 +1,232 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+using CSP.Client.Statics;
+using System.Runtime.Serialization.Json;
+using System.Net;
+
+namespace CSP.Client.Auth
+{
+    class Client
+    {
+        private readonly HttpClient _api;
+
+        AuthenticationContext context = new AuthenticationContext($"{CSPConfig.Authority}/{CSPConfig.ApplicationDomain}");
+
+        private async Task<string> GetToken()
+        {
+            var clientCred = new ClientCredential(CSPConfig.ApplicationId, CSPConfig.ApplicationSecret);
+            var token = await context.AcquireTokenAsync(CSPConfig.ResourceUrl, clientCred);
+            return token.AccessToken;
+        }
+
+        public Client()
+        {
+            _api = new HttpClient(new AuthenticatedHttpClientHandler(GetToken)) { BaseAddress = new Uri(CSPConfig.PartnerServiceApiRoot) };
+
+            _api.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer");
+        }
+
+
+        public async Task<string> Test()
+        {
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "v1/customers?size=40")
+            {
+                Headers = {
+                    { HttpRequestHeader.Accept.ToString(), "application/json" },
+                    //{ "MS-RequestId", Guid.NewGuid().ToString() },
+                    //{ "MS-CorrelationId", Guid.NewGuid().ToString() },
+                    //{ "MS-Contract-Version", "v1" },
+                }
+            };
+
+
+            var response = await _api.SendAsync(request);
+
+            var json = await response.Content.ReadAsStringAsync();
+            return json;
+        }
+
+
+        public async Task<string> TestReferrals()
+        {
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "partner/v1/analytics/referrals")
+            {
+                Headers = {
+                    { HttpRequestHeader.Accept.ToString(), "application/json" },
+                    { "MS-RequestId", Guid.NewGuid().ToString() },
+                    { "MS-CorrelationId", Guid.NewGuid().ToString() },
+                    { "MS-Contract-Version", "v1" },
+                }
+            };
+
+
+            var response = await _api.SendAsync(request);
+
+            var json = await response.Content.ReadAsStringAsync();
+            return json;
+        }
+
+
+        public async Task<string> TestIndirectresellers()
+        {
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "partner/v1/analytics/usage/azure")
+            {
+                Headers = {
+                    { HttpRequestHeader.Accept.ToString(), "application/json" },
+                    { "MS-RequestId", Guid.NewGuid().ToString() },
+                    { "MS-CorrelationId", Guid.NewGuid().ToString() },
+                    { "MS-Contract-Version", "v1" },
+                }
+            };
+
+
+            var response = await _api.SendAsync(request);
+
+            var json = await response.Content.ReadAsStringAsync();
+            return json;
+        }
+
+        public async Task<string> TestDeployment()
+        {
+
+            var id = "8a6b62ad-01cb-4d60-88a8-87a6490371d5";
+            var request = new HttpRequestMessage(HttpMethod.Get, $"v1/analytics/licenses/deployment")
+            {
+                Headers = {
+                    { HttpRequestHeader.Accept.ToString(), "application/json" },
+                    { "MS-RequestId", Guid.NewGuid().ToString() },
+                    { "MS-CorrelationId", Guid.NewGuid().ToString() },
+                    { "MS-Contract-Version", "v1" },
+                }
+            };
+
+
+            var response = await _api.SendAsync(request);
+
+            var json = await response.Content.ReadAsStringAsync();
+            return json;
+        }
+
+
+        public async Task<string> TestUsage()
+        {
+
+            var id = "8a6b62ad-01cb-4d60-88a8-87a6490371d5";
+            var request = new HttpRequestMessage(HttpMethod.Get, $"v1/analytics/licenses/usage")
+            {
+                Headers = {
+                    { HttpRequestHeader.Accept.ToString(), "application/json" },
+                    { "MS-RequestId", Guid.NewGuid().ToString() },
+                    { "MS-CorrelationId", Guid.NewGuid().ToString() },
+                    { "MS-Contract-Version", "v1" },
+                }
+            };
+
+
+            var response = await _api.SendAsync(request);
+
+            var json = await response.Content.ReadAsStringAsync();
+            return json;
+        }
+
+        public async Task<string> TestPartnerUsage()
+        {
+
+            var id = "8a6b62ad-01cb-4d60-88a8-87a6490371d5";
+            var request = new HttpRequestMessage(HttpMethod.Get, $"v1/analytics/licenses/usage")
+            {
+                Headers = {
+                    { HttpRequestHeader.Accept.ToString(), "application/json" },
+                    { "MS-RequestId", Guid.NewGuid().ToString() },
+                    { "MS-CorrelationId", Guid.NewGuid().ToString() },
+                    { "MS-Contract-Version", "v1" },
+                }
+            };
+
+
+            var response = await _api.SendAsync(request);
+
+            var json = await response.Content.ReadAsStringAsync();
+            return json;
+        }
+
+
+        // v1/analytics/commercial/deployment/license
+        public async Task<string> TestCommercialDeployment()
+        {
+
+            var id = "8a6b62ad-01cb-4d60-88a8-87a6490371d5";
+            var request = new HttpRequestMessage(HttpMethod.Get, $"v1/analytics/commercial/deployment/license")
+            {
+                Headers = {
+                    { HttpRequestHeader.Accept.ToString(), "application/json" },
+                    { "MS-RequestId", Guid.NewGuid().ToString() },
+                    { "MS-CorrelationId", Guid.NewGuid().ToString() },
+                    { "MS-Contract-Version", "v1" },
+                }
+            };
+
+
+            var response = await _api.SendAsync(request);
+
+            var json = await response.Content.ReadAsStringAsync();
+            return json;
+        }
+
+
+        public async Task<string> TestCommercialUsage()
+        {
+
+            var id = "8a6b62ad-01cb-4d60-88a8-87a6490371d5";
+            var request = new HttpRequestMessage(HttpMethod.Get, $"v1/analytics/commercial/usage/license")
+            {
+                Headers = {
+                    { HttpRequestHeader.Accept.ToString(), "application/json" },
+                    { "MS-RequestId", Guid.NewGuid().ToString() },
+                    { "MS-CorrelationId", Guid.NewGuid().ToString() },
+                    { "MS-Contract-Version", "v1" },
+                }
+            };
+
+
+            var response = await _api.SendAsync(request);
+
+            var json = await response.Content.ReadAsStringAsync();
+            return json;
+        }
+
+
+        public async Task<string> GetAllSearchAnalytics()
+        {
+
+            var id = "8a6b62ad-01cb-4d60-88a8-87a6490371d5";
+            var request = new HttpRequestMessage(HttpMethod.Get, $"partner/v1/analytics/search?top=100")
+            {
+                Headers = {
+                    { HttpRequestHeader.Accept.ToString(), "application/json" },
+                    { HttpRequestHeader.ContentType.ToString(), "application/json"},
+                    //{ "MS-CorrelationId", Guid.NewGuid().ToString() },
+                    //{ "MS-Contract-Version", "v1" },
+                    //{ "MS-RequestId", Guid.NewGuid().ToString()}
+                }
+            };
+
+
+            var response = await _api.SendAsync(request);
+
+            var json = await response.Content.ReadAsStringAsync();
+            return json;
+        }
+    }
+}
---
+++ CSP.Client/CSP.Client/Statics/CSPConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSP.Client.Statics
+{
+    /// <summary>
+    /// Partner Center operated by 21Vianet	
+    /// https://login.chinacloudapi.cn	
+    /// https://graph.chinacloudapi.cn	
+    /// https://partner.partnercenterapi.microsoftonline.cn
+    /// https://login.chinacloudapi.cn/a3267ba8-bb7b-49ed-9343-760eebbfb58c/wsfed
+    /// </summary>
+    public class CSPConfig
+    {
+        /// <summary>
+        /// please set your application setting here
+        /// </summary>
+        public static string PartnerServiceApiRoot = "https://partner.partnercenterapi.microsoftonline.cn";
+        public static string Authority = "https://login.chinacloudapi.cn";
+        public static string ResourceUrl = "https://graph.chinacloudapi.cn";
+        public static string ApplicationId = "b091199c-ced2-495a-b982-291dd455a191";
+        public static string ApplicationSecret = "";
+        public static string ApplicationDomain = "";
+    }
+}
