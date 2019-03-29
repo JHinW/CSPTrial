13087e7cc1db146769a4bbaa3af3869d1b885807
Wang Jin3 <wang.jin3@oe.21vianet.com>
2019-03-29 14:58:41 +0800

    jonb

---
+++ CSP.Client/Azure.JobTrial/test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.JobTrial
+{
+    class test
+    {
+
+        public static void test()
+        {
+            System.Security.Cryptography.X509Certificates.X509Certificate2()
+
+
+        }
+    }
+}
