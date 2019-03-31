499df2c2522a63fcfd142013e4f26906dd701616
wang jin <v-jiwag@hotmail.com>
2019-03-31 14:38:24 +0800

    web job as console

---
+++ CSP.Client/Azure.Job/Functions.cs
@@ -0,0 +1,23 @@
+﻿namespace Azure.JobTrial
+{
+    using Azure.JobTrial.Filters;
+    using Azure.JobTrial.Models;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Threading.Tasks;
+
+    [ErrorHandler]
+    public class Functions
+    {
+        [WorkItemValidator]
+        public async Task ProcessWorkItem([QueueTrigger("jiwagtest")] WorkItem workItem, ILogger logger)
+        {
+            // _sampleServiceA.DoIt();
+            logger.LogInformation($"Processing work item {workItem.ID}");
+            await Task.Delay(new TimeSpan(0, 20, 0));
+
+            logger.LogInformation($"Processed work item {workItem.ID}");
+        }
+    }
+}
---
+++ CSP.Client/Azure.Job/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Azure.Job
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .UseEnvironment("Development")
+                .ConfigureWebJobs(b =>
+                {
+                    b.AddAzureStorageCoreServices()
+                    .AddAzureStorage();
+                })
+                //.ConfigureAppConfiguration(b =>
+                //{
+                //    // Adding command line as a configuration source
+                //   // b.AddCommandLine(args);
+                //})
+                .ConfigureLogging((context, b) =>
+                {
+                    b.SetMinimumLevel(LogLevel.Debug);
+                    b.AddConsole();
+
+                    // If this key exists in any config, use it to enable App Insights
+
+                    //string appInsightsKey = context.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+                    //if (!string.IsNullOrEmpty(appInsightsKey))
+                    //{
+                    //    b.AddApplicationInsights(o => o.InstrumentationKey = appInsightsKey);
+                    //}
+                })
+                .ConfigureServices(services =>
+                {
+                    // add some sample services to demonstrate job class DI
+
+                    //services.AddSingleton<ISampleServiceA, SampleServiceA>();
+                    //services.AddSingleton<ISampleServiceB, SampleServiceB>();
+                })
+                .UseConsoleLifetime();
+
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
